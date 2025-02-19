6d8e70ca4e8e8d6b0ec662ed21b7d8c10983062f
MisaghMomeniB <jenabemisagh@gmail.com>
2025-02-19 16:11:14 +0330

    import library

--- analysis.py
+++ analysis.py
@@ -1,7 +1,3 @@
 import pandas as pd
 import matplotlib.pyplot as plt
-from textblob import TextBlob
-from collections import Counter
-import re
-from wordcloud import WordCloud
 import seaborn as sns
